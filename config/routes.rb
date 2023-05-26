57af5600ce0a93b43d87e0dfa88fb69f466bb17e
jessantillo <jessantillo@gmail.com>
2023-05-26 10:30:49 +0200

    (FEAT): Add Routes for Movies and Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "movies#index"
+  resources :movies do 
+    resources :reviews
+    collection { post :import }
+  end
+
+  resources :reviews do
+    collection { post :import }
+  end
 end
